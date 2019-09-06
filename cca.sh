379afc27bcadf9949d5ddc2890c62ee2c7c63bfa
Jagadesh Munta <jagadesh.munta@couchbase.com>
2019-09-06 10:52:26 -0700

    Added few more steps to cca

--- cca.sh
+++ cca.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
+#!/bin/bash -x
 #####################################################
 # Description: Code coverage analysis helper script
 #
 #####################################################
+
+clean()
+{
+  if [ -d $WORKSPACE/cbws ]; then
+     rm -rf $WORKSPACE/cbws
+  fi  
+}
+repo_sync()
+{
+  if [ ! -d $WORKSPACE/cbws ]; then
+    mkdir $WORKSPACE/cbws
+  fi
+  cd $WORKSPACE/cbws
+  repo init -u git://github.com/couchbase/manifest.git -m couchbase-server/mad-hatter.xml  -g all
+  repo sync
+}
 
 build_analytics()
 {
@@ -27,17 +43,40 @@ build_analytics()
     #Build:
     export MAVEN_OPTS=-Xmx4g
     #mvn -f analytics/cbas/pom.xml -B verify -P jenkins,cbas-cbcluster-test -pl :cbas-cbserver-test
-    mvn -f analytics/cbas/pom.xml -B verify -P cbas-cbcluster-test -pl :cbas-cbserver-test
-
+    mvn -f analytics/cbas/pom.xml -B verify -P cbas-cbcluster-test -pl :cbas-cbserver-test -Dmaven.repo.local=$CB_MAVEN_REPO_LOCAL
     
+}
+
+enable_jacoco()
+{
+  curl -v -u Administrator:password -X PUT -d "jvmArgs=-javaagent:/Users/jagadeshmunta/jacoco/jacocoagent.jar" http://localhost:8095/analytics/config/service
+}
+
+gen_jacoco_report()
+{
+  java -jar jacococli.jar report $HOME/jacoco.exec --xml ~/jacocoreports/jacoco.xml --csv ~/jacocoreports/jacoco.csv --html ~/jacocoreports/html --classfiles $HOME/.m2/com/couchbase/analytics/
+
+}
+
+run_scanner()
+{
+  ./sonar-scanner -Dsonar.projectKey=samplejacoco -Dsonar.host.url=http://172.23.105.131:9000 -Dsonar.login=5517d08da769a1fccd8551e6b4431d03baafd005 -Dsonar.coverage.jacoco.xmlReportPaths=/Users/jagadeshmunta/jacocoreports/jacoco.xml -Dsonar.sources=/Users/jagadeshmunta/cbws/analytics -Dsonar.dynamicAnalysis=reuseReports -Dsonar.java.coveragePlugin=jacoco -Dsonar.jacoco.reportMissing.force.zero=true -Dsonar.java.binaries=/Users/jagadeshmunta/.m2/repository/com/couchbase/analytics
+
+}
+
+run_sca()
+{
+   ~/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=analytics   -Dsonar.sources=.   -Dsonar.host.url=http://172.23.105.131:9000   -Dsonar.login=5517d08da769a1fccd8551e6b4431d03baafd005 -Dsonar.java.binaries=$HOME/.m2 -Dsonar.inclusions=**/*.java,**/*.go
 }
 
 all()
 {
-    export WORKSPACE=.
     #REPO=$WORKSPACE/.repository
-    REPO=$PWD
-    build_analytics $REPO
+    #REPO=$HOME/.m2
+    export WORKSPACE=$HOME
+    #clean
+    repo_sync
+    build_analytics $HOME/.m2
 }
 
 all
