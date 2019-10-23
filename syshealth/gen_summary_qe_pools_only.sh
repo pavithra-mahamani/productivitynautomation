ecac2317595035fda2084b590041f320e1167935
Jagadesh Munta <jagadesh.munta@couchbase.com>
2019-10-23 15:45:07 -0700

    Refactored for better display of the data

--- syshealth/gen_summary_qe_pools_only.sh
+++ syshealth/gen_summary_qe_pools_only.sh
@@ -16,25 +16,25 @@ fi
 OS_COUNT="`grep ${OS} vms_os_count.txt`"
 echo "Total VMs on ${OS_COUNT}"
 echo "----------------------------------------"
-echo "Summary of Server Pools on ${OS}"
+echo "Server pools list on ${OS}"
 echo "----------------------------------------"
 cat vmpools_${OS}_counts.txt
-echo "----------------------------------------"
-echo 'Pool Name : (TimedoutCount) UnreachableCount + OKCount = TotalCount'
 echo "----------------------------------------"
 TTIMED=0
 TUNREACH=0
 INDEX=1
 GRAND=0
+OUT_ALL_UNREACHABLE=unreachable_all_${NPOOL}.ini
+cat /dev/null>$OUT_ALL_UNREACHABLE
 for line in `cat vmpools_${OS}_counts.txt|cut -f1 -d':'`
 do
   POOL="$line"
   REQ_POOL="`grep -iw $POOL ${USED_POOLS}`"
   if [ ! "$REQ_POOL" = "" ]; then
-    echo "Pool: $POOL"
+    echo "$INDEX. Server Pool: $POOL"
     NPOOL="`echo $line|sed -e 's/ //' -e 's/-//g'`"
     LOG_FILE=ping_log_${OS}_${NPOOL}.txt
-    OUT_TIMEDOUT=${NPOOL}_unreachable.ini
+    OUT_UNREACHABLE=unreachable_${NPOOL}.ini
     #echo ansible ${NPOOL} -i vmpools_${OS}_ips.ini -u root -m ping
     if [ -f ~/.ansible_vars.ini ]; then
       VAR_FOUND="`grep 'ansible_user' vmpools_${OS}_ips.ini`"
@@ -53,14 +53,14 @@ do
     SUCCESS="`egrep SUCCESS ${LOG_FILE}| cut -f5 -d':' |cut -f5 -d' '|wc -l|xargs`"
     TOTAL="`egrep '=>' ${LOG_FILE}| cut -f5 -d':' |cut -f5 -d' '|wc -l|xargs`"
     echo " "
-    echo $INDEX. $POOL : '('$TIMEDOUT')' $UNREACH +$SUCCESS = $TOTAL
+    echo "  Total VMs:$TOTAL, Success:$SUCCESS, Unreachable:$UNREACH "'('Timedout:$TIMEDOUT')'
     TTIMED=`expr $TIMEDOUT + $TTIMED`
     TUNREACH=`expr $UNREACH + $TUNREACH`
     #if [ ! "${TIMEDOUT}" = "0" ]; then
     # echo "  Timedout VMs: "
     # TIMEDOUT_IPS="`egrep timed ${LOG_FILE} | cut -f4 -d':' |cut -f5 -d' '`"
-    # echo "[${NPOOL}_TIMEDOUT]" >${OUT_TIMEDOUT}
-    # egrep timed ${LOG_FILE} | cut -f4 -d':' |cut -f5 -d' ' >>${OUT_TIMEDOUT}
+    # echo "[${NPOOL}_TIMEDOUT]" >${OUT_UNREACHABLE}
+    # egrep timed ${LOG_FILE} | cut -f4 -d':' |cut -f5 -d' ' >>${OUT_UNREACHABLE}
     # for IP in `echo ${TIMEDOUT_IPS}`
     # do
     #     egrep $IP vms_list_${OS}_ips.ini
@@ -68,40 +68,46 @@ do
     #fi
     if [ ! "${SUCCESS}" = "0" ]; then
      SUCCESS_IPS="`egrep SUCCESS ${LOG_FILE} | cut -f1 -d' '`"
-     echo "  OK VMs:"
+     echo "  Success VMs:"
      I2=1
      for IP in `echo ${SUCCESS_IPS}`
      do
         IPQ=`echo ${IP}: |sed 's/\./\\\./g'`
         IPINFO="`egrep ${IPQ} vms_list_${OS}_ips.ini`"
-        echo "${I2}. $IPINFO : SUCCESS"
+        echo "   ${I2}. $IPINFO : SUCCESS"
         I2=`expr ${I2} + 1`
      done
     fi
     if [ ! "${UNREACH}" = "0" ]; then
      echo "  Unreachable VMs: "
      UNREACH_IPS="`egrep UNREACHABLE ${LOG_FILE} | cut -f1 -d' '`"
-     echo "[${NPOOL}_UNREACHABLE]" >${OUT_TIMEDOUT}
-     egrep UNREACHABLE ${LOG_FILE} | cut -f1 -d' '>>${OUT_TIMEDOUT}
+     echo "[${NPOOL}_UNREACHABLE]" >${OUT_UNREACHABLE}
+     egrep UNREACHABLE ${LOG_FILE} | cut -f1 -d' '>>${OUT_UNREACHABLE}
      for IP in `echo ${UNREACH_IPS}`
      do
          IPQ=`echo ${IP}: |sed 's/\./\\\./g'`
          IPINFO="`egrep ${IPQ} vms_list_${OS}_ips.ini`"
-         UNREACH_MSG="`egrep UNREACHABLE ${LOG_FILE} -A 3|egrep $IP | egrep msg|cut -f2- -d':'|cut -f1 -d','`"
-         echo "${I2}. $IPINFO : ${UNREACH_MSG}"
+         IPQ2="`echo ${IP} |sed 's/\./\\\./g'` "
+         UNREACH_MSG="`egrep UNREACHABLE ${LOG_FILE} -A 3|egrep ${IPQ2} | egrep msg|cut -f2- -d':'|cut -f1 -d','`"
+         echo "   ${I2}. $IPINFO : ${UNREACH_MSG}"
          I2=`expr ${I2} + 1`
      done
     fi
     echo " "
     INDEX=`expr $INDEX + 1`
     GRAND=`expr $GRAND + ${I2}`
+    if [ -f $OUT_UNREACHABLE ]; then
+        cat $OUT_UNREACHABLE >>${OUT_ALL_UNREACHABLE}
+        echo "" >>${OUT_ALL_UNREACHABLE}
+    fi
   fi
 done
 echo "----------------------------------------"
-FINAL_INI=required_final_timedout.ini
-echo "[TIMEDOUT]">${FINAL_INI}
-sort $OUT_TIMEDOUT|uniq >>${FINAL_INI}
+FINAL_INI=unreachablelist_ips.ini
+echo "[UNREACHABLE]">${FINAL_INI}
+sort $OUT_ALL_UNREACHABLE|uniq >>${FINAL_INI}
 UNIQUE=`cat ${FINAL_INI}|egrep -v '\[' |wc -l |xargs`
-echo Unique Timedout IPs: $UNIQUE '(Overall='$TTIMED')'
+echo Unique unreachable IPs: $UNIQUE '(Overall unreachable:'$TUNREACH')' '(Total:'$GRAND')'
 
 echo Please check ${FINAL_INI} file for exact IPs.
+date
