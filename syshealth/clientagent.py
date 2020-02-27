ce3bb331e9280d18b5368d49d72dc8d44a2dfe67
Jagadesh Munta <jagadesh.munta@couchbase.com>
2020-02-26 17:00:27 -0800

    Refactored and added more data from the windows vm

--- syshealth/clientagent.py
+++ syshealth/clientagent.py
@@ -1,24 +1,29 @@
-import socket, time
+import socket, time, uuid, platform
 
 
 server = '10.100.172.116'
 port = 12345
 is_connect = False
-while not is_connect:
+while True:
+  is_connect = False
+  time.sleep(2)
+  while not is_connect:
     try:
-        print("Connecting...")
+        #print("Connecting...")
         client_socket = socket.socket()
         client_socket.connect((server,port))
         is_connect = True
 
     except:
-        print("Retrying...")
+        #print("Retrying...")
         time.sleep(2)
         continue
 
-host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-send_msg = "{ \"" + host_name + "\" : \"" + host_ip+"\"}"
-print("ClientAgent: Sending "+send_msg)
-client_socket.send(send_msg)
-client_socket.close()
+  host_name = socket.gethostname()
+  host_ip = socket.gethostbyname(host_name)
+  macaddress = hex(uuid.getnode())
+  platform = platform.platform()
+  send_msg = macaddress+","+host_name +","+host_ip+","+platform
+  print("ClientAgent: Sending "+send_msg)
+  client_socket.send(send_msg)
+  client_socket.close()
--- syshealth/serveragent.py
+++ syshealth/serveragent.py
@@ -1,29 +1,38 @@
 import socket, select
 
-port = 12345
-socket_list = []
-users = {}
+class ServerAgent:
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('',port))
-server_socket.listen(5)
-socket_list.append(server_socket)
+    def __init__(self, server_port=12345):
+        self.port = server_port
+        self.socket_list = []
+        self.users = {}
 
-is_exit = False
-print("ServerAgent: Waiting for clients...")
-while not is_exit:
-    ready_to_read,ready_to_write,in_error = select.select(socket_list,[],[],0)
-    for sock in ready_to_read:
-        if sock == server_socket:
-            connect, addr = server_socket.accept()
-            socket_list.append(connect)
-            connect.send("You are connected from:" + str(addr))
-        else:
-            try:
-                data = sock.recv(2048)
-                print("Received data: "+data)
-                is_exit = True
-                break
-            except:
-                continue
+    def get_ip(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(('',self.port))
+        server_socket.listen(5)
+        self.socket_list.append(server_socket)
+
+        is_exit = False
+        print("ServerAgent: Waiting for clients...")
+        while not is_exit:
+            ready_to_read,ready_to_write,in_error = select.select(self.socket_list,[],[],0)
+            for sock in ready_to_read:
+                if sock == server_socket:
+                    connect, addr = server_socket.accept()
+                    self.socket_list.append(connect)
+                else:
+                    try:
+                        data = sock.recv(2048)
+                        #print("Received data: "+data)
+                        return data
+                    except:
+                        continue
+
+
+def main():
+    print(ServerAgent().get_ip())
+
+if __name__ == "__main__":
+    main()
