901b785675f0fc0fb47a9aae1fba1b72704462b0
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2020-11-02 14:06:37 +0000

    Regex include and exclude

--- jenkins/hanging_jobs.py
+++ jenkins/hanging_jobs.py
@@ -6,6 +6,7 @@ from pytz import timezone
 import requests
 from optparse import OptionParser
 import logging
+import re
 
 logger = logging.getLogger("hanging_jobs")
 logger.setLevel(logging.DEBUG)
@@ -86,21 +87,12 @@ def parse_arguments():
     parser.add_option("-u", "--url", dest="build_url_to_check",
                       default='http://qa.sc.couchbase.com', help="Build URL to check")
     parser.add_option("-t", "--timeout", dest="timeout", help="No console output timeout (minutes)", default=60, type="int")
-    parser.add_option("-e", "--exclude", dest="exclude", help="List of job names to exclude")
-    parser.add_option("-i", "--include", dest="include", help="List of job names to include")
+    parser.add_option("-e", "--exclude", dest="exclude", help="Regular expression of job names to exclude")
+    parser.add_option("-i", "--include", dest="include", help="Regular expression of job names to include")
     parser.add_option("-p", "--print", dest="print", help="Just print hanging jobs, don't stop them", action="store_true")
 
     options, args = parser.parse_args()
 
-    if options.exclude:
-        options.exclude = options.exclude.split(",")
-    else:
-        options.exclude = []
-
-    if options.include:
-        options.include = options.include.split(",")
-    else:
-        options.include = []
 
     if options.build_url_to_check:
         build_url_to_check = options.build_url_to_check
@@ -118,11 +110,11 @@ def parse_arguments():
 
 def stop_hanging_jobs(server, hanging_jobs, include, exclude):
     for job in hanging_jobs:
-        if len(options.include) > 0 and job['name'] not in options.include:
+        if options.include and not re.search(options.include, job['name']):
             logger.info("Skipping {}, not included".format(job['name']))
             continue
 
-        if job['name'] in options.exclude:
+        if options.exclude and re.search(options.exclude, job['name']):
             logger.info("Skipping {}, excluded".format(job['name']))
             continue
         
