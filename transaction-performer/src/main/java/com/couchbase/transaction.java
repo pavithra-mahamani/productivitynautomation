a3ff90bd6c6fbb3971a3a8c15d6fff15d2114763
s_sridevi <s_sridevi15@yahoo.com>
2020-12-16 09:29:06 -0800

    fixed according to PR reviews

--- transaction-performer/src/main/java/com/couchbase/transaction.java
+++ transaction-performer/src/main/java/com/couchbase/transaction.java
@@ -1,11 +1,11 @@
 package com.couchbase;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.Collection;
 import com.couchbase.client.java.json.JsonObject;
-import com.couchbase.client.java.json.JsonArray;
 import com.couchbase.transactions.TransactionGetResult;
 import com.couchbase.transactions.Transactions;
 import com.couchbase.transactions.config.TransactionConfigBuilder;
@@ -16,37 +16,31 @@ public class transaction {
     private final static String CONTENT_NAME= "content";
     private final static String CLUSTER_LOGIN_USERNAME= "Administrator";
     private final static String CLUSTER_LOGIN_PASSWORD= "password";
-    //private final static JsonObject INITIAL;
-    //private final static JsonObject UPDATED = JsonObject.create().put(CONTENT_NAME, "updated");
     private static JsonObject doccontent = JsonObject.create();
     private static String clusterIP;
     private static String bucket;
     private static String docstring;
-    private static String[] docidArray;
-    private static String[] removeDocidArray;
+    private static ArrayList<String> operationList = new ArrayList<String>();;
     private static String temp_str;
     private static JsonObject temp_obj;
-    private static String removeDocIds;
- 
+    private static String[] optDocidArray;
+    private static String operationDocids;
+    private static ArrayList<TxnOperationId> txnOptObj = new ArrayList<TxnOperationId>();
+    private static TxnOperationId txnobj;
+    private static  String opt;
+    
+   
     public static void main(String[] args)  {
         String randomId = UUID.randomUUID().toString();
         String docId = null;
-        String operation = "insert";
         for(String parameter : args) {
 			switch (parameter.split("=")[0].toLowerCase()) {
                 case "clusterip":
                     clusterIP= parameter.split("=")[1];
                     break;
-                case "operation":
-                    operation = parameter.split("=")[1];
-                    break;
-                case "docid":
-                    docId = parameter.split("=")[1];
-                    docidArray = docId.split(",");
-                    break;
-                case "removedocids":
-                    removeDocIds = parameter.split("=")[1];
-                    removeDocidArray = removeDocIds.split(",");
+                case "operationdocids":
+                    operationDocids = parameter.split("=")[1];
+                    optDocidArray = operationDocids.split(",");
                     break;
                 case "bucket":
                 	bucket = parameter.split("=")[1];
@@ -56,7 +50,7 @@ public class transaction {
                 	doccontent = JsonObject.fromJson(docstring);              	
                 	break;
                 default:
-                    System.out.println("Unknown parameter is given as input. Exiting" + parameter);
+                    System.out.println("Unknown parameter is given as input. Exiting " + parameter);
                     Usage();
                     System.exit(-1);
             }
@@ -65,7 +59,18 @@ public class transaction {
         	doccontent = JsonObject.create().put(CONTENT_NAME, "initial");
         	doccontent.put("id", randomId);
         }
-
+        
+        
+        for(int i=0;i<optDocidArray.length;i++) {
+        	txnobj= new TxnOperationId();
+        	opt = optDocidArray[i].split("-")[0];
+        	txnobj.txnOperation = opt;
+        	txnobj.txnDocID = optDocidArray[i].split("-")[1];
+        	txnOptObj.add(txnobj);
+        	if(!operationList.contains(opt))
+        		operationList.add(opt);
+        }
+        
         try{
 
             //Cluster on which transactions are to be executed
@@ -76,23 +81,21 @@ public class transaction {
 
             TransactionConfigBuilder builder = TransactionConfigBuilder.create();  // Default transaction Configuration
             Transactions transactions = Transactions.create(cluster, builder);  // Transaction factory with default config is created on the cluster
-            String[] operationList = operation.split(",");
-            String finalDocId = docId;
             
-           /* if(finalOperation.equals("insert")){
-                finalDocId = randomId;
-            }*/
+            String finalDocId = docId;
             
             transactions.run((ctx) -> {
                 TransactionGetResult result ;
-                for(int j=0;j<operationList.length;j++) {
-                switch(operationList[j]){
-                    case "insert":
-                    	for(int i=0; i<docidArray.length; i++) {
-                    		doccontent.put("id", docidArray[i]);
-                        	doccontent.put("content", "doc_content"+docidArray[i]);
-                    		ctx.insert(collectionForTxnOperations,docidArray[i],doccontent);
-                    	}	
+                for(int j=0;j<operationList.size();j++) {
+                switch(operationList.get(j)){
+                    case "insert":                    	
+                    	for(int i=0; i<txnOptObj.size(); i++) {
+                    		if(txnOptObj.get(i).txnOperation.equals("insert")) {
+	                    		doccontent.put("id", txnOptObj.get(i).txnDocID);
+	                        	doccontent.put("content", "doc_content"+txnOptObj.get(i).txnDocID);
+	                    		ctx.insert(collectionForTxnOperations,txnOptObj.get(i).txnDocID,doccontent);
+                    		}	
+                    	}
                         break;
                     case "get":
                         ctx.get(collectionForTxnOperations, finalDocId);
@@ -103,34 +106,32 @@ public class transaction {
                         ctx.getOptional(collectionForTxnOperations, finalDocId);
                         break;
                     case "replace":
-                    	for(int i=0; i<docidArray.length; i++) {	
-                    		result =  ctx.get(collectionForTxnOperations, docidArray[i]);
-                    		temp_obj = result.contentAsObject();
-                    		temp_str = temp_obj.get("id").toString();
-                    		temp_obj.put("id", temp_str+"_updated");
-                    		temp_str = temp_obj.get("content").toString();
-                    		temp_obj.put("content", temp_str+"_updated");
-                            ctx.replace(result,temp_obj);
-                    	}         
+                    	for(int i=0; i<txnOptObj.size(); i++) {
+                    		if(txnOptObj.get(i).txnOperation.equals("replace")) {
+                    			result =  ctx.get(collectionForTxnOperations, txnOptObj.get(i).txnDocID);
+                        		temp_obj = result.contentAsObject();
+                        		temp_str = temp_obj.get("id").toString();
+                        		temp_obj.put("id", temp_str+"_updated");
+                        		temp_str = temp_obj.get("content").toString();
+                        		temp_obj.put("content", temp_str+"_updated");
+                        		ctx.replace(result,temp_obj);
+                    		}	
+                    	}
                         break;
                     case "remove":
-                    	for(int i=0; i<removeDocidArray.length; i++) {	
-                    		result =  ctx.get(collectionForTxnOperations, removeDocidArray[i]);
-                    		ctx.remove(result);
-                    	}    
+                    	for(int i=0; i<txnOptObj.size(); i++) {
+                    		if(txnOptObj.get(i).txnOperation.equals("remove")) {
+                    			result =  ctx.get(collectionForTxnOperations, txnOptObj.get(i).txnDocID);
+                        		ctx.remove(result);
+                    		}	
+                    	}
                         break;
                     case "commit":
                         ctx.commit();
                         break;
                     case "rollback":
-                    	for(int i=0; i<docidArray.length; i++) {
-                    		doccontent.put("id", docidArray[i]);
-                        	doccontent.put("content", "doc_content"+docidArray[i]);
-                    		ctx.insert(collectionForTxnOperations,docidArray[i],doccontent);
-                    	}
                         ctx.rollback();
                         break;
-                    
                 }
             }
             });
@@ -146,6 +147,12 @@ public class transaction {
     private static void Usage(){
         System.out.println("\n Usage: \n");
         System.out.println("Please enter the command in below format: \n");
-        System.out.println("java -cp <Relative location to this Jar> com.couchbase.transaction  clusterIp=<YourClusterIp> operation=<RequiredTransactionOperation> docId=<DocumentId>  \n");
+        System.out.println("java -cp <Relative location to this Jar> com.couchbase.transaction  clusterIp=<YourClusterIp> operationDocids=<RequiredTransactionOperation> docId=<DocumentId>  \n");
     }
+    
+    private static class TxnOperationId{
+    	String txnOperation=null, txnDocID=null;
+    }
+
 }
+
