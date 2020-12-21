931c291b2c59e8135617b9e4f0b719943f58ba55
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2020-12-21 13:44:52 +0000

    allBuilds is too heavy

--- jenkins/rerun_failed_jobs.py
+++ jenkins/rerun_failed_jobs.py
@@ -254,25 +254,16 @@ def get_duplicate_jobs(running_builds, job_name, parameters, options):
 
 
 def latest_jenkins_builds(options):
-    jobs_to_check = ["test_suite_executor", "test_suite_executor-dynvm", "test_suite_executor-jython", "test_suite_executor-TAF"]
     latest_builds = []
-
-    for job_to_check in jobs_to_check:
-        while True:
-            try:
-                response = requests.get("{}/job/{}/api/json?tree=allBuilds[url,number,actions[parameters[name,value]]]".format(options.jenkins_url, job_to_check)).json()
-            except Exception:
-                time.sleep(5)
-            else:
-                for build in response["allBuilds"]:
-                    parameters = parameters_from_actions(build["actions"])
-                    latest_builds.append({
-                        "name": job_to_check,
-                        "number": build['number'],
-                        "parameters": parameters
-                    })
-                break
-
+    response = requests.get(options.jenkins_url + "/api/json?tree=jobs[url,name,builds[url,number,actions[parameters[name,value]]]]").json()
+    for job in response["jobs"]:
+        for build in job["builds"]:
+            parameters = parameters_from_actions(build["actions"])
+            latest_builds.append({
+                "name": job['name'],
+                "number": build['number'],
+                "parameters": parameters
+            })
     return latest_builds
 
 
