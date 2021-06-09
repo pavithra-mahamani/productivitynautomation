7d2835180e189da029860f73321cce3ee8242c42
Jagadesh Munta <jagadesh.munta@couchbase.com>
2021-06-09 01:39:40 -0700

    Adding cpu,memory,disk info

--- syshealth/check_pool_vm_health.py
+++ syshealth/check_pool_vm_health.py
@@ -45,22 +45,23 @@ def get_pool_data(pools):
         ssh_ok = 0
         index = 0
         csvout = open("pool_vm_health_info.csv", "w")
-        print("ipaddr,ssh_status,ssh_error,os,pool_state,pool_ids")
-        csvout.write("ipaddr,ssh_status,ssh_error,os,pool_state,pool_ids")
+        print("ipaddr,ssh_status,ssh_error,os,pool_state,pool_ids,cpus,memory_total,memory_free,memory_available,disk_size,disk_used,disk_avail,disk_use%")
+        csvout.write("ipaddr,ssh_status,ssh_error,os,pool_state,pool_ids,cpus,memory_total,memory_free,memory_available,disk_size,disk_used,disk_avail,disk_use%")
         for row in result:
             index += 1
             try:
-                ssh_status, ssh_error = check_vm(row['os'],row['ipaddr'])
+                ssh_status, ssh_error, cpus, meminfo, diskinfo = check_vm(row['os'],row['ipaddr'])
                 if ssh_status == 'ssh_failed':
                     ssh_failed += 1
                 else:
                     ssh_ok += 1
-                print("{},{},{},{},{},{},{}".format(index, row['ipaddr'], ssh_status, ssh_error, row['os'], \
-                    row['state'],  '+'.join("{}".format(p) for p in row['poolId'])))
-                csvout.write("\n{},{},{},{},{},{}".format(row['ipaddr'], ssh_status, ssh_error, row['os'], \
-                    row['state'],  '+'.join("{}".format(p) for p in row['poolId'])))
+                print("{},{},{},{},{},{},{},{},{},{}".format(index, row['ipaddr'], ssh_status, ssh_error, row['os'], \
+                    row['state'],  '+'.join("{}".format(p) for p in row['poolId']), cpus, meminfo, diskinfo))
+                csvout.write("\n{},{},{},{},{},{},{},{},{}".format(row['ipaddr'], ssh_status, ssh_error, row['os'], \
+                    row['state'],  '+'.join("{}".format(p) for p in row['poolId']), cpus, meminfo, diskinfo))
                 csvout.flush()
-            except:
+            except Exception as ex:
+                print(ex)
                 pass
             count +=1
         print("ssh_ok={},ssh_failed={},total={}".format(ssh_ok, ssh_failed,count))
@@ -87,11 +88,41 @@ def check_vm(os_name, host):
             password=password,
             timeout=30
         )
+        cpus = get_cpuinfo(client)
+        meminfo = get_meminfo(client)
+        diskinfo = get_diskinfo(client)
         client.close()
     except Exception as e:
-        print(e)
-        return 'ssh_failed', str(e).replace(',',' ')
-    return 'ssh_ok', ''
+        meminfo = ',,'
+        diskinfo = ',,,'
+        return 'ssh_failed', str(e).replace(',',' '), '', meminfo, diskinfo
+    return 'ssh_ok', '', cpus, meminfo, diskinfo
+
+def get_cpuinfo(ssh_client):
+    return ssh_command(ssh_client,"cat /proc/cpuinfo  |egrep processor |wc -l")
+
+def get_meminfo(ssh_client):
+    return ssh_command(ssh_client,"cat /proc/meminfo |egrep Mem |cut -f2- -d':'|sed 's/ //g'|xargs|sed 's/ /,/g'")
+
+def get_diskinfo(ssh_client):
+    return ssh_command(ssh_client,"df -hl --output=size,used,avail,pcent / |tail -1 |sed 's/ \+/,/g'|cut -f2- -d','")
+
+def ssh_command(ssh_client, cmd):
+    ssh_output = ''
+    ssh_error = ''
+    try:
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh_client.exec_command(cmd)
+        
+        for line in iter(ssh_stdout.readline, ""):
+            ssh_output += line
+        if ssh_output:
+            ssh_output = str(ssh_output).rstrip()
+        for line in iter(ssh_stderr.readline, ""):
+            ssh_error += line
+    except:
+        print("cmd={},error={}".format(cmd,ssh_error))
+
+    return ssh_output
 
 def main():
     if len(sys.argv) < 2:
