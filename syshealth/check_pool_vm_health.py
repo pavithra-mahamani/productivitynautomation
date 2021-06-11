3cbbabd1cadbaa07af45ad4438395366eb4b66b1
Jagadesh Munta <jagadesh.munta@couchbase.com>
2021-06-10 17:16:05 -0700

    Adding individual couchbase service statuses as indepedent columns.

--- syshealth/check_pool_vm_health.py
+++ syshealth/check_pool_vm_health.py
@@ -47,24 +47,24 @@ def get_pool_data(pools):
         csvout = open("pool_vm_health_info.csv", "w")
         print("ipaddr,ssh_status,ssh_error,os,pool_state,pool_ids,cpus,memory_total(kB),memory_free(kB),memory_available(kB),memory_use(%)," + \
                 "disk_size(MB),disk_used(MB),disk_avail(MB),disk_use%,uptime,system_time,users,cpu_load_avg_1min,cpu_load_avg_5mins,cpu_load_avg_15mins," + \
-                "total_processes,couchbase_process,couchbase_version,couchbase_services")
+                "total_processes,couchbase_process,couchbase_version,couchbase_services,cb_data_kv_status,cb_index_status,cb_query_status,cb_search_status,cb_analytics_status,cb_eventing_status,xdcr_status")
         csvout.write("ipaddr,ssh_status,ssh_error,os,pool_state,pool_ids,cpus,memory_total(kB),memory_free(kB),memory_available(kB),memory_use(%)," + \
                 "disk_size(MB),disk_used(MB),disk_avail(MB),disk_use%,uptime,system_time,users,cpu_load_avg_1min,cpu_load_avg_5mins,cpu_load_avg_15mins," \
-                "total_processes,couchbase_process,couchbase_version,couchbase_services")
+                "total_processes,couchbase_process,couchbase_version,couchbase_services,cb_data_kv_status,cb_index_status,cb_query_status,cb_search_status,cb_analytics_status,cb_eventing_status,xdcr_status")
         for row in result:
             index += 1
             try:
-                ssh_status, ssh_error, cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_proc, cb_proc, cb_version, cb_serv = check_vm(row['os'],row['ipaddr'])
+                ssh_status, ssh_error, cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_proc, cb_proc, cb_version, cb_serv, cb_ind_serv = check_vm(row['os'],row['ipaddr'])
                 if ssh_status == 'ssh_failed':
                     ssh_state=0
                     ssh_failed += 1
                 else:
                     ssh_state=1
                     ssh_ok += 1
-                print("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(index, row['ipaddr'], ssh_status, ssh_error, row['os'], \
-                    row['state'],  '+'.join("{}".format(p) for p in row['poolId']), cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_proc, cb_proc, cb_version, cb_serv))
-                csvout.write("\n{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(row['ipaddr'], ssh_state, ssh_error, row['os'], \
-                    row['state'],  '+'.join("{}".format(p) for p in row['poolId']), cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_proc, cb_proc, cb_version, cb_serv))
+                print("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(index, row['ipaddr'], ssh_status, ssh_error, row['os'], \
+                    row['state'],  '+'.join("{}".format(p) for p in row['poolId']), cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_proc, cb_proc, cb_version, cb_serv, cb_ind_serv))
+                csvout.write("\n{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(row['ipaddr'], ssh_state, ssh_error, row['os'], \
+                    row['state'],  '+'.join("{}".format(p) for p in row['poolId']), cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_proc, cb_proc, cb_version, cb_serv, cb_ind_serv))
                 csvout.flush()
             except Exception as ex:
                 print(ex)
@@ -113,8 +113,28 @@ def check_vm(os_name, host):
             meminfo += ","+ str(round(((int(mem_total)-int(mem_free))/int(mem_total))*100))
         while len(cpu_load.split(','))<4:
             cpu_load += ','
-        #while len(cb_running_serv.split(','))<7:
-        #    cb_running_serv += ','
+        cb_serv_data = 0
+        cb_serv_index = 0
+        cb_serv_query = 0
+        cb_serv_search = 0
+        cb_serv_analytics = 0
+        cb_serv_eventing = 0
+        cb_serv_xdcr = 0
+        if 'data' in cb_running_serv:
+            cb_serv_data = 1
+        if 'index' in cb_running_serv:
+            cb_serv_index = 1
+        if 'query' in cb_running_serv:
+            cb_serv_query = 1
+        if 'search' in cb_running_serv:
+            cb_serv_search = 1
+        if 'analytics' in cb_running_serv:
+            cb_serv_analytics = 1
+        if 'eventing' in cb_running_serv:
+            cb_serv_eventing = 1
+        if 'xdcr' in cb_running_serv:
+            cb_serv_xdcr = 1
+        cb_ind_serv = "{},{},{},{},{},{},{}".format(cb_serv_data, cb_serv_index, cb_serv_query, cb_serv_search, cb_serv_analytics, cb_serv_eventing, cb_serv_xdcr)
     
         client.close()
     except Exception as e:
@@ -122,8 +142,9 @@ def check_vm(os_name, host):
         diskinfo = ',,,'
         cpu_load = ',,,'
         cb_running_serv=','
-        return 'ssh_failed', str(e).replace(',',' '), '', meminfo, diskinfo,'','',cpu_load, '', '', '',cb_running_serv
-    return 'ssh_ok', '', cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_total_processes, cb_processes, cb_version, cb_running_serv
+        cb_ind_serv = ',,,,,,'
+        return 'ssh_failed', str(e).replace(',',' '), '', meminfo, diskinfo,'','',cpu_load, '', '', '',cb_running_serv, cb_ind_serv
+    return 'ssh_ok', '', cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_total_processes, cb_processes, cb_version, cb_running_serv, cb_ind_serv
 
 def get_cpuinfo(ssh_client):
     return ssh_command(ssh_client,"cat /proc/cpuinfo  |egrep processor |wc -l")
