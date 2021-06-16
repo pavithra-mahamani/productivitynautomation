d962530e08a4a78ec3017fefc2652962fdaef27e
Jagadesh Munta <jagadesh.munta@couchbase.com>
2021-06-15 19:51:02 -0700

    Added parallel vm checks and switched it for better performance.

--- syshealth/check_pool_vm_health.py
+++ syshealth/check_pool_vm_health.py
@@ -16,6 +16,8 @@ from datetime import timedelta
 from couchbase.auth import PasswordAuthenticator
 from couchbase.cluster import Cluster, ClusterOptions, ClusterTimeoutOptions
 from paramiko import SSHClient, AutoAddPolicy
+import multiprocessing as mp
+
 
 def get_pool_data(pools):
     pools_list = []
@@ -93,6 +95,96 @@ def get_pool_data(pools):
         print(fex)
         #print("exception:", sys.exc_info()[0])
 
+
+def get_pool_data_parallel(pools):
+    pools_list = []
+    for pool in pools.split(','):
+        pools_list.append(pool)
+    
+    query = "SELECT ipaddr, os, state, origin, poolId, username FROM `QE-server-pool` WHERE poolId in [" \
+                + ', '.join('"{0}"'.format(p) for p in pools_list) + "] or " \
+                + ' or '.join('"{0}" in poolId'.format(p) for p in pools_list)
+    pool_cb_host = os.environ.get('pool_cb_host')
+    if not pool_cb_host:
+        pool_cb_host = "172.23.104.162"
+    pool_cb_user = os.environ.get('pool_cb_user')
+    if not pool_cb_user:
+        pool_cb_user = "Administrator"
+    pool_cb_user_p = os.environ.get('pool_cb_password')
+    if not pool_cb_user_p:
+        print("Error: pool_cb_password environment variable setting is missing!")
+        exit(1)
+    try:
+        pool_cluster = Cluster("couchbase://"+pool_cb_host, ClusterOptions(PasswordAuthenticator(pool_cb_user, pool_cb_user_p),
+        timeout_options=ClusterTimeoutOptions(kv_timeout=timedelta(seconds=10))))
+        result = pool_cluster.query(query)
+        
+        csvout = open("pool_vm_health_info.csv", "w")
+        print("ipaddr,ssh_status,ssh_error,pool_os,real_os,os_match_state,pool_state,pool_ids,pool_user,cpus,memory_total(kB),memory_free(kB),memory_available(kB),memory_use(%)," + \
+                "disk_size(MB),disk_used(MB),disk_avail(MB),disk_use%,uptime,system_time,users,cpu_load_avg_1min,cpu_load_avg_5mins,cpu_load_avg_15mins," + \
+                "total_processes,couchbase_process,couchbase_version,couchbase_services,cb_data_kv_status,cb_index_status,cb_query_status,cb_search_status,cb_analytics_status,cb_eventing_status,cb_xdcr_status")
+        csvout.write("ipaddr,ssh_status,ssh_error,pool_os,real_os,os_match_state,pool_state,pool_ids,pool_user,cpus,memory_total(kB),memory_free(kB),memory_available(kB),memory_use(%)," + \
+                "disk_size(MB),disk_used(MB),disk_avail(MB),disk_use%,uptime,system_time,users,cpu_load_avg_1min,cpu_load_avg_5mins,cpu_load_avg_15mins," \
+                "total_processes,couchbase_process,couchbase_version,couchbase_services,cb_data_kv_status,cb_index_status,cb_query_status,cb_search_status,cb_analytics_status,cb_eventing_status,cb_xdcr_status")
+        
+        mp_pool = mp.Pool(mp.cpu_count())
+        data = mp_pool.map(get_pool_data_vm_parallel, [row for row in result])
+        mp_pool.close()
+
+        count = 0
+        ssh_failed = 0
+        ssh_ok = 0
+        for r in data:
+            count += 1
+            ssh_status=r.split(',')[1]
+            if int(ssh_status) == 1:
+                ssh_ok += 1
+            else:
+                ssh_failed += 1
+            print("{},{}".format(count,r))
+            csvout.write("\n{}".format(r))
+            csvout.flush()
+            
+
+        booked_count = get_pool_state_count(pool_cluster, pools_list, 'booked')
+        avail_count = get_pool_state_count(pool_cluster, pools_list, 'available')
+        using_count = booked_count + avail_count
+        print("ssh_ok={},ssh_failed={},total={},booked={},avail={},using={}".format(ssh_ok, ssh_failed,count, booked_count, avail_count, using_count))
+        csvout.close()
+    except Exception as fex :
+        print(fex)
+        #print("exception:", sys.exc_info()[0])
+
+def get_pool_data_vm_parallel(row):    
+    try:
+        os_mappings={"centos":"centos linux 7 (core)", "centosnonroot":"centos linux 7 (core)", "debian10":"debian gnu/linux 10 (buster)", \
+                    "oel8":"oracle linux server 8.1", "rhel":"red hat enterprise linux", "rhel8":"red hat enterprise linux 8.3 (ootpa)", \
+                    "suse12":"suse linux enterprise server 12 sp2", "opensuse15":"opensuse leap 15.1","suse15":"suse linux enterprise server 15", \
+                    "opensuse15hostname":"opensuse leap 15.1","suse15hostname":"suse linux enterprise server 15","ubuntu18":"ubuntu 18.04.4 lts", \
+                    "ubuntu20":"ubuntu 20.04 lts" }
+
+        ssh_status, ssh_error, real_os, cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_proc, cb_proc, cb_version, cb_serv, cb_ind_serv = check_vm(row['os'],row['ipaddr'])
+        if ssh_status == 'ssh_failed':
+            ssh_state=0
+        else:
+            ssh_state=1
+        
+        os_state = 0
+        if real_os:
+            pool_os = row['os'].lower()
+            if pool_os in os_mappings.keys() and os_mappings[pool_os] == real_os.lower():
+                os_state = 1
+            elif pool_os in os_mappings.keys() and pool_os.startswith('suse15'):
+                if os_mappings['open'+pool_os] == real_os.lower():
+                    os_state = 1
+            
+        return "{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(row['ipaddr'], ssh_state, ssh_error, row['os'], real_os, \
+            os_state, row['state'],  '+'.join("{}".format(p) for p in row['poolId']), row['username'], cpus, meminfo, diskinfo, uptime, systime, cpu_load, cpu_proc, cb_proc, cb_version, cb_serv, cb_ind_serv)
+        
+    except Exception as ex:
+        print(ex)
+        pass
+    
 def get_pool_state_count(pool_cluster, pools_list, pool_state):
     query = "SELECT count(*) as count FROM `QE-server-pool` WHERE state='" + pool_state + "' and (poolId in [" \
                 + ', '.join('"{0}"'.format(p) for p in pools_list) + "] or " \
@@ -261,7 +353,8 @@ def main():
     xen_hosts_file = ''
     if len(sys.argv) > 2:
         xen_hosts_file = sys.argv[2]
-    get_pool_data(pools)
+    #get_pool_data(pools)
+    get_pool_data_parallel(pools)
 
 
 if __name__ == '__main__':
